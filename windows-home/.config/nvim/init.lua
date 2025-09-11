89cef1227a66a38718a1a34856c841e57ea60cc2
rbuchss <rbuchss@gmail.com>
2025-09-10 22:35:02 -0400

    move to init.lua for neovim

---
+++ windows-home/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+-- Set runtime path to include ~/vimfiles directories (Windows paths)
+vim.opt.runtimepath:prepend("~/vimfiles")
+vim.opt.runtimepath:append("~/vimfiles/after")
+
+-- Set packpath to match runtimepath
+vim.opt.packpath = vim.opt.runtimepath:get()
+
+-- Source the existing vimrc file (Windows path)
+vim.cmd.source('~/_vimrc')
