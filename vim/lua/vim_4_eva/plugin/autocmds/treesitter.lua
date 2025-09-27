5eb020ae8f67185dbeed68b6cc85d39b7420f040
rbuchss <rbuchss@gmail.com>
2025-09-26 20:52:14 -0400

    update treesitter settings

--- vim/lua/vim_4_eva/plugin/autocmds/treesitter.lua
+++ vim/lua/vim_4_eva/plugin/autocmds/treesitter.lua
@@ -1,13 +1,6 @@
 local M = {}
 
 function M.setup(config)
-  -- Disable treesitter for help files since it cannot parse them correctly
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "help",
-    callback = function()
-      vim.treesitter.stop()
-    end,
-  })
 end
 
 return M
--- vim/lua/vim_4_eva/plugin/settings/treesitter.lua
+++ vim/lua/vim_4_eva/plugin/settings/treesitter.lua
@@ -1,13 +1,42 @@
 local M = {}
 
 function M.setup(config)
-  -- make sure that all installed parsers are updated to the latest version via :TSUpdate
-  -- TODO: make this not happen every load
-  vim.cmd.TSUpdate()
+  -- Make sure that all installed parsers are updated to the latest version via :TSUpdate
+  -- TODO: make this not happen every load possibly with some debouncing.
+  --
+  -- For now , just disabling and relying on manually running this.
+  --
+  -- Note that if enabled with auto_install=true this errors with using the same tmp dirs.
+  --
+  -- vim.cmd.TSUpdate()
 
   require('nvim-treesitter.configs').setup({
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = {
+      "bash",
+      -- "c",
+      "diff",
+      "lua",
+      "vim",
+      "vimdoc",
+      -- "query",
+      "markdown",
+      "markdown_inline",
+    },
+
+    -- Install parsers asynchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
+
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = {},
+
     highlight = {
       enable = true,
+
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
