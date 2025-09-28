c2fc3378894eafe584d13fc197eb177fdda4a595
Russ Buchanan <rbuchss@gmail.com>
2025-09-27 21:24:51 -0400

    breakout lsp autocmd and diagnostic settings (#38)

--- vim/lua/vim_4_eva/plugin/autocmds/init.lua
+++ vim/lua/vim_4_eva/plugin/autocmds/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup(config)
+  require('vim_4_eva.plugin.autocmds.lsp').setup(config)
   require('vim_4_eva.plugin.autocmds.treesitter').setup(config)
   require('vim_4_eva.plugin.autocmds.yank').setup(config)
 end
