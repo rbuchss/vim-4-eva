5fc622fdf39b56ae2e72af53f35cf4e873bf2281
rbuchss <rbuchss@gmail.com>
2025-09-29 21:24:26 -0400

    clean up neovim package config

--- windows-home/.config/nvim/init.lua
+++ windows-home/.config/nvim/init.lua
@@ -7,3 +7,10 @@ vim.opt.packpath = vim.opt.runtimepath:get()
 
 -- Source the existing vimrc file (Windows path)
 vim.cmd.source('~/_vimrc')
+
+require('vim_4_eva.pack').setup({
+  packages = 'all',
+})
+
+require('vim_4_eva.plugin').setup({})
+require('vim_4_eva.ftplugin').setup({})
