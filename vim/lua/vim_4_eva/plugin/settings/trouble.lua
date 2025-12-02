e7903728c197f38e66efb66361104bb3216f247a
rbuchss <rbuchss@gmail.com>
2025-12-01 20:24:19 -0500

    add trouble

---
+++ vim/lua/vim_4_eva/plugin/settings/trouble.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+function M.setup(_)
+  require('vim_4_eva.pack').lazy.register({
+    'trouble.nvim',
+    cmd = {
+      'Trouble',
+    },
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    after = function()
+      require('trouble').setup({})
+    end,
+  })
+
+end
+
+return M
