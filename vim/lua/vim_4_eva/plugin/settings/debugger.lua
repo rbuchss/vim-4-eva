e69f705dca6ca5e12fe29db11793f29f40f9850c
rbuchss <rbuchss@gmail.com>
2025-12-04 12:12:54 -0500

    allow debugger view selection and improve keymaps

--- vim/lua/vim_4_eva/plugin/settings/debugger.lua
+++ vim/lua/vim_4_eva/plugin/settings/debugger.lua
@@ -1,6 +1,48 @@
 local M = {
   loaded = {},
+  current_layouts = {},
+  -- Common layout parameters for individual element layouts
+  single_element_size = 20,
+  single_element_position = 'bottom',
 }
+
+-- Helper function to switch layouts
+-- layout_nums can be a single number or a table of numbers
+M.switch_layout = function(layout_nums)
+  local dapui = require('dapui')
+
+  -- Normalize to table
+  if type(layout_nums) ~= 'table' then
+    layout_nums = {layout_nums}
+  end
+
+  -- Close all currently open layouts
+  for _, layout in ipairs(M.current_layouts) do
+    dapui.close({ layout = layout })
+  end
+
+  -- Check if we're toggling off the same layout(s)
+  local is_same = #M.current_layouts == #layout_nums
+  if is_same then
+    for i, layout in ipairs(layout_nums) do
+      if M.current_layouts[i] ~= layout then
+        is_same = false
+        break
+      end
+    end
+  end
+
+  if is_same then
+    -- Toggle off
+    M.current_layouts = {}
+  else
+    -- Open new layout(s)
+    for _, layout in ipairs(layout_nums) do
+      dapui.open({ layout = layout })
+    end
+    M.current_layouts = layout_nums
+  end
+end
 
 M.filetype_callbacks = {
   any = function()
@@ -22,6 +64,50 @@ M.filetype_callbacks = {
 }
 
 function M.setup(_)
+  --[[ DEBUG KEYBINDINGS REFERENCE
+
+  Execution Control:
+    <leader>dc, <F5>     - Continue/Start debugging
+    <leader>dC, <S-F5>   - Run Last
+    <leader>dT, <C-F5>   - Terminate
+    <leader>dR, <M-F5>   - Restart
+    <leader>dt           - Run to Cursor
+    <leader>dp           - Pause
+    <leader>do, <F10>    - Step Over
+    <leader>di, <F11>    - Step Into
+    <leader>dO, <F12>    - Step Out
+
+  Breakpoints:
+    <leader>db, <F9>     - Toggle Breakpoint
+    <leader>dB, <S-F9>   - Set Conditional Breakpoint
+    <leader>dl, <C-F9>   - Set Log Point
+    <leader>dX           - Clear All Breakpoints
+
+  UI - Single Element Layouts (bottom, toggle):
+    <leader>dus, <S-F8>  - Toggle Scopes
+    <leader>duc, <C-F8>  - Toggle Console
+    <leader>dur, <F8>    - Toggle REPL
+    <leader>dub, <M-F8>  - Toggle Breakpoints
+    <leader>duk, <C-S-F8>- Toggle Stacks
+    <leader>duw, <M-C-F8>- Toggle Watches
+
+  UI - Floating Windows (80% screen, centered):
+    <leader>duS          - Float Scopes
+    <leader>duC          - Float Console
+    <leader>duR          - Float REPL
+    <leader>duB          - Float Breakpoints
+    <leader>duK          - Float Stacks
+    <leader>duW          - Float Watches
+
+  UI - Classic View:
+    <leader>dui, <M-S-F8>- Toggle Classic UI (sidebar + bottom)
+
+  Evaluation & Inspection:
+    <leader>de           - Evaluate Expression (normal/visual mode)
+    <leader>dk           - Stack Frame Up
+    <leader>dj           - Stack Frame Down
+  ]]--
+
   local map = function(keys, func, desc, mode)
     mode = mode or 'n'
 
@@ -80,19 +166,156 @@ function M.setup(_)
   )
 
   map(
-    {'<leader>dur', '<F8>'},
+    '<leader>dt',
     function()
-      require('dap').repl.toggle()
+      require('dap').run_to_cursor()
     end,
-    'REPL Toggle'
+    'Run to Cursor'
   )
 
   map(
-    {'<leader>dui', '<S-F8>'},
+    '<leader>dp',
     function()
-      require('dapui').toggle()
+      require('dap').pause()
     end,
-    'UI Toggle'
+    'Pause'
+  )
+
+  map(
+    '<leader>de',
+    function()
+      require('dapui').eval()
+    end,
+    'Evaluate Expression',
+    {'n', 'v'}
+  )
+
+  map(
+    '<leader>dk',
+    function()
+      require('dap').up()
+    end,
+    'Stack Frame Up'
+  )
+
+  map(
+    '<leader>dj',
+    function()
+      require('dap').down()
+    end,
+    'Stack Frame Down'
+  )
+
+  map(
+    {'<leader>dur', '<F8>'},
+    function()
+      M.switch_layout(3)
+    end,
+    'Toggle REPL'
+  )
+
+  map(
+    {'<leader>dus', '<S-F8>'},
+    function()
+      M.switch_layout(1)
+    end,
+    'Toggle Scopes'
+  )
+
+  map(
+    {'<leader>duc', '<C-F8>'},
+    function()
+      M.switch_layout(2)
+    end,
+    'Toggle Console'
+  )
+
+  map(
+    {'<leader>dub', '<M-F8>'},
+    function()
+      M.switch_layout(4)
+    end,
+    'Toggle Breakpoints'
+  )
+
+  map(
+    {'<leader>duk', '<C-S-F8>'},
+    function()
+      M.switch_layout(5)
+    end,
+    'Toggle Stacks'
+  )
+
+  map(
+    {'<leader>duw', '<M-C-F8>'},
+    function()
+      M.switch_layout(6)
+    end,
+    'Toggle Watches'
+  )
+
+  map(
+    {'<leader>dui', '<M-S-F8>'},
+    function()
+      M.switch_layout({7, 8})
+    end,
+    'Toggle Classic UI'
+  )
+
+  -- Floating window variants (80% of screen)
+  local float_opts = {
+    width = math.floor(vim.o.columns * 0.8),
+    height = math.floor(vim.o.lines * 0.8),
+    enter = true,
+    position = "center",
+  }
+
+  map(
+    '<leader>duS',
+    function()
+      require('dapui').float_element('scopes', float_opts)
+    end,
+    'Float Scopes'
+  )
+
+  map(
+    '<leader>duC',
+    function()
+      require('dapui').float_element('console', float_opts)
+    end,
+    'Float Console'
+  )
+
+  map(
+    '<leader>duR',
+    function()
+      require('dapui').float_element('repl', float_opts)
+    end,
+    'Float REPL'
+  )
+
+  map(
+    '<leader>duB',
+    function()
+      require('dapui').float_element('breakpoints', float_opts)
+    end,
+    'Float Breakpoints'
+  )
+
+  map(
+    '<leader>duK',
+    function()
+      require('dapui').float_element('stacks', float_opts)
+    end,
+    'Float Stacks'
+  )
+
+  map(
+    '<leader>duW',
+    function()
+      require('dapui').float_element('watches', float_opts)
+    end,
+    'Float Watches'
   )
 
   map(
@@ -117,6 +340,14 @@ function M.setup(_)
       require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
     end,
     'Set Log Point Breakpoint'
+  )
+
+  map(
+    '<leader>dX',
+    function()
+      require('dap').clear_breakpoints()
+    end,
+    'Clear All Breakpoints'
   )
 
   map(
@@ -210,19 +441,73 @@ function M.setup(_)
           },
 
           layouts = {
+            -- Layout 1: Scopes (default)
+            {
+              elements = {
+                { id = 'scopes', size = 1.0 },
+              },
+              size = M.single_element_size,
+              position = M.single_element_position,
+            },
+            -- Layout 2: Console
             {
               elements = {
                 { id = 'console', size = 1.0 },
               },
-              size = 5,                        -- height in lines (adjust to taste)
-              position = 'bottom',             -- "left", "right", "top", "bottom"
+              size = M.single_element_size,
+              position = M.single_element_position,
             },
+            -- Layout 3: REPL
             {
               elements = {
-                { id = 'scopes', size = 1.0 }, -- other options: repl, console
+                { id = 'repl', size = 1.0 },
               },
-              size = 15,                       -- height in lines (adjust to taste)
-              position = 'bottom',             -- "left", "right", "top", "bottom"
+              size = M.single_element_size,
+              position = M.single_element_position,
+            },
+            -- Layout 4: Breakpoints
+            {
+              elements = {
+                { id = 'breakpoints', size = 1.0 },
+              },
+              size = M.single_element_size,
+              position = M.single_element_position,
+            },
+            -- Layout 5: Stacks
+            {
+              elements = {
+                { id = 'stacks', size = 1.0 },
+              },
+              size = M.single_element_size,
+              position = M.single_element_position,
+            },
+            -- Layout 6: Watches
+            {
+              elements = {
+                { id = 'watches', size = 1.0 },
+              },
+              size = M.single_element_size,
+              position = M.single_element_position,
+            },
+            -- Layout 7: Classic view - Left sidebar
+            {
+              elements = {
+                { id = 'scopes', size = 0.25 },
+                { id = 'breakpoints', size = 0.25 },
+                { id = 'stacks', size = 0.25 },
+                { id = 'watches', size = 0.25 },
+              },
+              size = 40,
+              position = 'left',
+            },
+            -- Layout 8: Classic view - Bottom panel
+            {
+              elements = {
+                { id = 'repl', size = 0.5 },
+                { id = 'console', size = 0.5 },
+              },
+              size = 10,
+              position = 'bottom',
             },
           },
         })
@@ -245,9 +530,20 @@ function M.setup(_)
           vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
         end
 
-        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-        dap.listeners.before.event_exited['dapui_config'] = dapui.close
+        -- Only open layout 1 (scopes) by default
+        dap.listeners.after.event_initialized['dapui_config'] = function()
+          M.switch_layout(1)
+        end
+
+        dap.listeners.before.event_terminated['dapui_config'] = function()
+          dapui.close()
+          M.current_layouts = {}
+        end
+
+        dap.listeners.before.event_exited['dapui_config'] = function()
+          dapui.close()
+          M.current_layouts = {}
+        end
       end,
     },
     {
