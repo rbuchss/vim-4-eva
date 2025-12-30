01eb8d46c207cb2ba10bad1782edc9f84c027203
Russ Buchanan <rbuchss@gmail.com>
2025-12-29 22:53:18 -0500

    add more formatters for conform nvim (#85)

--- vim/lua/vim_4_eva/plugin/settings/conform.lua
+++ vim/lua/vim_4_eva/plugin/settings/conform.lua
@@ -7,22 +7,22 @@ function M.setup(_)
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>fb',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = '[F]ormat [B]uffer',
       },
     },
     after = function()
       require('conform').setup({
         notify_on_error = false,
         format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- Disable 'format_on_save lsp_fallback' for languages that don't
           -- have a well standardized coding style. You can add additional
           -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
+          local disable_filetypes = {}
 
           if disable_filetypes[vim.bo[bufnr].filetype] then
             return nil
@@ -34,12 +34,21 @@ function M.setup(_)
           end
         end,
         formatters_by_ft = {
+          bash = { 'shfmt' },
+          c = { 'clang-format' },
+          cpp = { 'clang-format' },
+          cuda = { 'clang-format' },
+          go = { 'goimports', 'gofmt' },
+          -- You can use 'stop_after_first' to run the first available formatter from the list
+          javascript = { 'prettierd', 'prettier', stop_after_first = true },
           lua = { 'stylua' },
+          markdown = { 'markdownlint' },
           -- Conform can also run multiple formatters sequentially
           python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
-          --
-          -- You can use 'stop_after_first' to run the first available formatter from the list
-          -- javascript = { "prettierd", "prettier", stop_after_first = true },
+          -- You can also customize some of the format options for the filetype
+          rust = { 'rustfmt', lsp_format = 'fallback' },
+          sh = { 'shfmt' },
+          yaml = { 'yamlfmt' },
         },
       })
     end,
