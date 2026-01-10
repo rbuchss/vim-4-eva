edb5f492397c393fd80663ca88c37596e3d7a191
Russ Buchanan <rbuchss@gmail.com>
2026-01-09 21:02:33 -0500

    allow hidden files in telescope (#88)

--- vim/lua/vim_4_eva/plugin/settings/telescope.lua
+++ vim/lua/vim_4_eva/plugin/settings/telescope.lua
@@ -45,13 +45,18 @@ function M.setup(_)
         require('telescope').setup({
           -- You can put your default mappings / updates / etc. in here
           --  All the info you're looking for is in `:help telescope.setup()`
-          --
-          -- defaults = {
-          --   mappings = {
-          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-          --   },
-          -- },
-          -- pickers = {}
+          defaults = {
+            -- Ignore .git directory when showing hidden files
+            file_ignore_patterns = { '%.git/' },
+          },
+          pickers = {
+            find_files = {
+              hidden = true,
+            },
+            live_grep = {
+              additional_args = { '--hidden' },
+            },
+          },
           extensions = {
             ['ui-select'] = {
               require('telescope.themes').get_dropdown(),
@@ -67,9 +72,15 @@ function M.setup(_)
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sF', function()
+          builtin.find_files({ no_ignore = true })
+        end, { desc = '[S]earch all [F]iles (incl. gitignored)' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sG', function()
+          builtin.live_grep({ additional_args = { '--no-ignore' } })
+        end, { desc = '[S]earch by [G]rep (incl. gitignored)' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
